989f7fb79e76ab2bcce67697d08334dd2957a3f7
secxsast <134115670+secxsast@users.noreply.github.com>
2023-11-21 12:32:23 -0600

    Update AdminAPI.java

--- AdminAPI.java
+++ AdminAPI.java
@@ -1,6 +1,6 @@
 //  Copyright (c) 2009, RSA, The Security Division of EMC
 //          All Rights Reserved.klsaksacxsastAscantesta
-//testing_dach20
+//testing_dach
 package com.rsa.samples.admin;
 
 import java.util.ArrayList;
